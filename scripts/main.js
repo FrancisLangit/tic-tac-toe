878ee62c706a4140671d6c5f11bd881049d55d45
Francis Langit <francis.villanueva.langit@gmail.com>
2021-04-26 10:12:16 +0800

    Construct features to display gameBoard.tiles
    gameBoard methods createTileDiv() and display() were created.

--- scripts/main.js
+++ scripts/main.js
@@ -2,16 +2,29 @@ let gameBoard = (function() {
     let tiles = [
         'X', ' ', ' ',
         'X', 'O', 'O',
-        'O', 'X', 'O',
+        'O', 'O', 'O',
     ];
     let container = document.querySelector('.gameBoardContainer');
+
+    function createTileDiv(tilesArrayElement) {
+        let tileDiv = document.createElement('div');
+        tileDiv.innerHTML += tilesArrayElement;
+        return tileDiv;
+    }
+
+    function display() {
+        for (i = 0; i < tiles.length; i++) {
+            container.append(createTileDiv(tiles[i]));
+        }
+    }
+
+    return {display}
 })();
 
 gameBoard.display();
 
-
 // Create array to hold element representing tiles on gameboard
 // Create container to hold grid of gameboard
 // For each element in gameboard's tiles:
-    // Style the element
+    // Create a node element out of the array element 
     // Append the element to the gameboard's container
