e093af464336aea791d4b4500d38ee75921f9cab
Francis Langit <francis.villanueva.langit@gmail.com>
2021-04-27 19:05:30 +0800

    Create reset button that resets game board

--- scripts/gameBoard.js
+++ scripts/gameBoard.js
@@ -1,5 +1,24 @@
 import { createTile } from './createTile.js';
 
+// Clear the game board in the UI
+// Reset the tiles array (perhaps redeclare it with "Array(9).fill(0)"?)
+// Display is different depending on status of game board:
+    // If draw/winner present: display "Play again?" 
+    // Else: display "Restart?"
+
+const resetButton = (() =>{    
+
+    const _resetGame = () => {
+        let gameBoardTiles = document.querySelectorAll('.gameBoardTile');
+        for (let i = 0; i < gameBoardTiles.length; i++) {
+            gameBoardTiles[i].innerHTML = '';
+        }
+        gameBoard.tiles = Array(9).fill(0);
+    }
+
+    let resetButtonNode = document.querySelector('.resetButton')
+    resetButtonNode.addEventListener('click', _resetGame);
+})();
 
 const gameBoard = (() => {
     /**Module representing gameboard on user interface where the bulk of the 
@@ -12,14 +31,17 @@ const gameBoard = (() => {
     const _isGameWon = (winValue) => {
         /**Returns true if a player has won. Otherwise, returns false.*/
         return (
+            // Check rows.
             tiles[0] + tiles[1] + tiles[2] === winValue ||
             tiles[3] + tiles[4] + tiles[5] === winValue ||
             tiles[6] + tiles[7] + tiles[8] === winValue ||
 
+            // Check columns.
             tiles[0] + tiles[3] + tiles[6] === winValue ||
             tiles[1] + tiles[4] + tiles[7] === winValue ||
             tiles[2] + tiles[5] + tiles[8] === winValue ||
 
+            // Check diagonals.
             tiles[0] + tiles[4] + tiles[8] === winValue ||
             tiles[6] + tiles[4] + tiles[2] === winValue
         )
