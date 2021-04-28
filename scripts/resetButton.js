8abade2886031679e7d3cbe4cbc98d6327e97b6e
Francis Langit <francis.villanueva.langit@gmail.com>
2021-04-28 16:24:36 +0800

    Add styling to webpage
    Also made resetButton reset back to default innerHTML when pressed.

--- scripts/resetButton.js
+++ scripts/resetButton.js
@@ -2,10 +2,12 @@ import { gameBoard } from './gameBoard.js';
 
 const resetButton = (() =>{    
     /**Represents reset button below game board that resets the game.*/
+    let resetButtonNode = document.querySelector('.resetButton')
 
     const _resetGame = () => {
         /**Clears all tiles in gameBoard, resets winnerDisplay, resets 
-         * gameBoard.currentUser, and resets gameBoard.tiles.*/
+         * gameBoard.currentUser, and resets gameBoard.tiles. Also resets 
+         * inner HTML of resetButton itself.*/
         let gameBoardTiles = document.querySelectorAll('.gameBoardTile');
         for (let i = 0; i < gameBoardTiles.length; i++) {
             gameBoardTiles[i].innerHTML = '';
@@ -13,10 +15,10 @@ const resetButton = (() =>{
         document.querySelector('.winnerDisplay').innerHTML = 'Game ongoing.';
         gameBoard.currentUser = 'X';
         gameBoard.tiles = Array(9).fill(0);
+        resetButtonNode.innerHTML = 'Restart?';
     }
 
     // Add event listener to reset button's node in document.
-    let resetButtonNode = document.querySelector('.resetButton')
     resetButtonNode.addEventListener('click', _resetGame);
 
     return {resetButtonNode}
