62fe93d57b9b1c7938606645ae2d90faa3bbce69
Francis Langit <francis.villanueva.langit@gmail.com>
2021-04-30 18:15:34 +0800

    Add line under imports in resetButton.js

--- scripts/resetButton.js
+++ scripts/resetButton.js
@@ -1,5 +1,6 @@
 import { main } from './main.js'; 
 
+
 const resetButton = (() =>{    
     /**Represents reset button below game board that resets the game.*/
     let resetButtonNode = document.querySelector('.resetButton')
